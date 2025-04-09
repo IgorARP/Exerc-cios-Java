01d332b96840370ff7a85c3abc207d8f6eb82780
igor palmieri <idupalmieri@hotmail.com>
2025-04-09 10:31:08 -0300

    Aula Array

---
+++ src/NivelBasico/Array.java
@@ -0,0 +1,15 @@
+package NivelBasico;
+
+public class Array {
+    public static void main(String[] args) {
+
+        String[] nome = new String[4];
+        nome[0] = "igor";
+        nome[1] = "joao";
+        nome[2] = "cleber";
+        nome[3] = "jonas";
+
+        System.out.println(nome[3]);
+
+    }
+}
